7d07c6b1485d29be6d396d094e16f2fcb0a02ac5
Ashraful Islam Mahim <ashrafulmahim@gmail.com>
2020-12-06 22:02:01 +0600

    static build file added for production

---
+++ static/build/precache-manifest.b12db0786d4e47d5e0260c3528f28b21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c34488cff09ea0c017d1a7e8b48feb20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f2ffe463c6b9dc2d085",
+    "url": "/static/css/main.8ac0b633.chunk.css"
+  },
+  {
+    "revision": "ec56669b2a34c66dadce",
+    "url": "/static/js/2.435f5a50.chunk.js"
+  },
+  {
+    "revision": "306f3bf9d2f1ee8399ad560a7731d0d6",
+    "url": "/static/js/2.435f5a50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f2ffe463c6b9dc2d085",
+    "url": "/static/js/main.32769629.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);

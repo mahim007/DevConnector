ca029f0e4ef32c37a48cf4e02ab59cd01eaee935
Ashraful Islam Mahim <ashrafulmahim@gmail.com>
2020-12-06 22:02:48 +0600

    path change to static/build from client/build

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("static/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "static", "build", "index.html"));
   });
 }
 
